339ead78d71020c0262274597ead38802eef9e09
Way2SelfAware <122562668+Way2SelfAware@users.noreply.github.com>
2023-04-22 10:18:09 -0700

    add reverse array

---
+++ reverse-array.js
@@ -0,0 +1,6 @@
+// set a const inputArray= Vaule
+const inputArray = [1, -1, 2, -3, 5, -8, 13];
+//assign the value of inputArray to either [1, 2, 3], [1, 3, 5, 7, 9, 11], [20, 50, 30, 60, 200], [1, -1, 2, -3, 5, -8, 13]
+// use array.reverse() to reverse the array
+const reversedArray = inputArray.reverse();
+console.log(reversedArray);
